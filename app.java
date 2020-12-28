9ea7da24659ff839b247ba57c4e3e29c64e7bfd2
Max Rydahl Andersen <max@xam.dk>
2020-12-28 10:26:17 +0100

    try add default app

---
+++ app.java
@@ -0,0 +1,15 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+import static java.lang.System.*;
+
+public class app {
+
+    public static void main(String... args) {
+        out.println("Welcome to jbang.dev\n");
+        out.println("This is a default jbang application hosted a https://jbang.dev/.");
+        out.println("It is not really how jbang.dev is best viewed, but congratulations");
+        out.println("on having jbang installed.\n");
+        out.println("All the best from jbang.dev and its awesome contributors!\n");
+        out.println("https://github.com/jbangdev/jbang/blob/master/CONTRIBUTORS.md");
+    }
+}
