0eb7e1b4ef67468979896e895d8bc2440c936cd0
Max Rydahl Andersen <max@xam.dk>
2021-05-29 13:59:44 +0200

    fix generated command

--- .github/appstore.java
+++ .github/appstore.java
@@ -168,7 +168,7 @@ class appstore implements Callable<Integer> {
     StringBuffer cmd = aliasToCommand(ghContent, item.alias, item.repoName, item.repoOwner);
 
     item.command = cmd.toString();
-    item.fullcommand = "jbang init -t " + item.command + " app.java";
+    item.fullcommand = "jbang " + item.command + " app.java";
 
     
     return item;
